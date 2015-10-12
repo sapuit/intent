da35ec82b34105a36750b7bfe0ee0ac1256857c4
sap <sapnkp@gmail.com>
2015-10-12 18:01:12 +0700

    intent demo 1.0

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("", "");
         Log.i("", "");
-
+        Log.i("", "");
 
     }
 
