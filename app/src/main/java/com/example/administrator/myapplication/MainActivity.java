f79df64acbad3dc6f6634b83cfb435b0d75197ee
sap <sapnkp@gmail.com>
2015-10-12 18:10:23 +0700

    intent demo 1.0

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -12,9 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("", "");
-        Log.i("", "");
-        Log.i("", "");
+
 
     }
 
